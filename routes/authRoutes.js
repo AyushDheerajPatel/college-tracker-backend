fec227ff75d6e63b4d0f09db66dd2ff608f23702
Ayush Patel <ayushdheerajpatel@gmail.com>
2026-08-03 21:29:10 +0530

    Switched Nodemailer to port 587 to fix ETIMEDOUT

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,8 +12,8 @@ const createTransporter = () => {
   if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     return nodemailer.createTransport({
       host: 'smtp.gmail.com',
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
